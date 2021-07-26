d2296b69027cb71aec07f75e06eb83946014c31e
olowookere marvellous <olowomarve@gmail.com>
2021-07-26 11:02:50 -0700

    complete searchbar

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import "./sidebar.css";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
+import {Avatar, IconButton} from "@material-ui/core";
 
 function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
+                <Avatar src="https://avatars.githubusercontent.com/u/67744592?v=4"/>
                 <div className="sidebar__headerRight">
-                    <DonutLargeIcon />
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="search or start new chat" type="text" />    
                 </div>
             </div>
         </div>
