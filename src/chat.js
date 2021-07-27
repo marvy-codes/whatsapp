4b426a1a134e0d5690ba2df68779cdf16006be7d
olowookere marvellous <olowomarve@gmail.com>
2021-07-27 15:28:19 -0700

    progress on  chat

--- src/chat.js
+++ src/chat.js
@@ -2,8 +2,10 @@ import React from 'react';
 import "./chat.css";
 import {Avatar, IconButton} from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 
 function Chat() {
+    
     return (
         <div className="chat">
             <div className="chat__header">
@@ -33,7 +35,25 @@ function Chat() {
                         {new Date().toUTCString()}
                     </span> 
                 </p>
+
+                <p className="chat__message chat__reciever">
+                    <span className="chat__name">sonny</span>
+                    Hi, how are you today
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span> 
+                </p>
                  
+            </div> 
+
+            <div className="chat__footer">
+                <InsertEmoticonIcon />
+                <form>
+                    <input placeholder="Type a message" type="text" />
+                    <button type="submit">
+                        send message
+                    </button>
+                </form>
             </div>
         </div>
     )
