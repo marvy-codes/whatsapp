dd2efea983ca68bd4c7a3e75b4fe5784d2c7979f
olowookere marvellous <olowomarve@gmail.com>
2021-08-02 14:16:37 -0700

    added pusher

--- src/App.js
+++ src/App.js
@@ -2,8 +2,20 @@ import React from "react";
 import './App.css';
 import Chat from "./chat.js";
 import Sidebar from "./sidebar.js";
+import Pusher from 'pusher-js'
 
 function App() {
+
+  useEffect(() => {
+    const pusher = new Pusher('fc05cdd3f53432f79111', {
+      cluster: 'mt1'
+    });
+    const channel = pusher.subscribe('messages');
+      channel.bind('inserted', function(data) {
+        alert(JSON.stringify(data))
+    })
+  }, [ ]);
+
   return (
     <div className="app">
       <div className="app__body">
