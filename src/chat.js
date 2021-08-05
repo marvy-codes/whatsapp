9e88dbd49bcb7c666cdb879807f5da728ce1a4dc
olowookere marvellous <olowomarve@gmail.com>
2021-08-05 04:56:57 -0700

    added axiox to chat.js

--- src/chat.js
+++ src/chat.js
@@ -3,8 +3,22 @@ import "./chat.css";
 import {Avatar, IconButton} from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import axios from './axios';
 
 function Chat({messages}) {
+    const [input, setInput] = useState("");
+
+    const sendMessage = async (e) => {
+        e.preventDefault();
+
+        await axios.post('/messages/new', {
+            message: input,
+            name: "Demo App",
+            timestamp: "just now",
+            received: false
+        });
+        setInput('')
+    };
     
     return (
         <div className="chat">
@@ -41,8 +55,8 @@ function Chat({messages}) {
             <div className="chat__footer">
                 <InsertEmoticonIcon />
                 <form>
-                    <input placeholder="Type a message" type="text" />
-                    <button type="submit">
+                    <input value={input} onChange={e => setInput(e.target.value)} placeholder="Type a message" type="text" />
+                    <button onClick={sendMessage} type="submit">
                         send message
                     </button>
                 </form>
